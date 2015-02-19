7617e869920328b74cb3613c507674a80e203cff
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-19 14:19:17 +0000

    added table

--- app/server.rb
+++ app/server.rb
@@ -23,10 +23,21 @@ class BattleShips < Sinatra::Base
       @message = "You didn't enter a name"
       erb :registration
     else
-      @player1 = params[:Name]
+      player1 = Player.new(name: params[:Name])
+      @player1 = player1.name
       erb :registration
     end
+  end
 
+  get '/play' do
+    @seed_data = grid
+    erb :play
+  end
+
+  post '/play' do
+    coordinate = params[:coordinate]
+    grid.shoot_at(coordinate)
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
